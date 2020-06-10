1a832fae89b21af2f99626078b981b6ac4fb9e7b
Tobias Iven <tobiasiven@gmail.com>
2020-06-10 10:10:47 -0700

    Adding colors and io connectiviity

--- app.r
+++ app.r
@@ -23,12 +23,23 @@ charnames <- c("Cartman", "Stan", "Kyle", "Butters", "Randy", "Mr. Garrison", "C
 
 #importing all p mats and fw prob
 
+expl1 <- "This is a model that generates speech from each character based on their lines on the show. 
+It randomly chooses a first word to start a sentence with based on what their lines begin with, 
+and then looks at all the words they have said after that word and randomly 
+(but proportionally chooses a next word. This repeats until a word is reached that always ended a line on the 
+show (no positive probabilities for a next word) or the sentence reaches the user defined max number of words. 
+This type of model is known as a Markov Chain. It does not do a very good job :(."
+expl2 <- "I also developed a two-word Markov Chain that looked at every unique pair of words and then generated a likely word based on that. 
+This gave much more realistic events but relied on a probability matrix that was ~1.2 gigabytes (These matrices are extremely sparse, 
+and I really should have been able to wildly reduce this but ran out of time). The code for this model is in “modelbuilding.Rmd” as well 
+as the code for one word Markov. Run it at your own (and your RAM’s) risk."
+expl3 <- "For more check out this guys awesome Markov model for generating Trump tweets. He is much better at building efficient, working models than I am.
+https://filiph.github.io/markov/"
 
-
-
+ntopchar = 10
  fws <- list()
  ps <- list()
- for (i in 1:10){
+for (i in 1:10){
    for (i in 1:ntopchar){
      filestring <- paste("pmats/", charnames[i],"onewordm.csv", sep = "")
 
@@ -38,10 +49,10 @@ charnames <- c("Cartman", "Stan", "Kyle", "Butters", "Randy", "Mr. Garrison", "C
 
      fws[[i]] <- read_csv(filestring)
    }
- }
+}
 uniqs <- list()
 for (i in 1:ntopchar){
-  filestring <- paste("pmats/", topcounts$Character[i],"uniqs.csv", sep = "")
+  filestring <- paste("pmats/", charnames[i],"uniqs.csv", sep = "")
   
   uniqs[[i]] <- read_csv(filestring)
 }
@@ -68,7 +79,7 @@ firstword <- function(charnum){
 
 nextword <- function(charnum, word){
   p = runif(1)
-  row = which.max(uniqs[[4]] == word)
+  row = which.max(uniqs[[charnum]] == word)
   iter = 1
   sums = 0
   while (sums < p){
@@ -82,17 +93,15 @@ nextword <- function(charnum, word){
   return(uniqs[[charnum]][loc,1])
 }
 
-generate <- function(characterchoice, length){
-  subtry <- fws[[3]]
-  senlen = length
-  print(characterchoice)
+generate <- function(characterchoice, len){
+  senlen = len
   senlist = list()
   
   senlist[1] = firstword(characterchoice)
   
-  row = match(senlist[1], uniq[[characterchoice]])
+  myrow = which.max(uniqs[[characterchoice]] == senlist[1])
   
-  sumrow = sum(ps[[characterchoice]][row,])
+  sumrow = sum(ps[[characterchoice]][myrow,])
   
   iter = 1
   
@@ -105,11 +114,17 @@ generate <- function(characterchoice, length){
   
   senlist <- as.matrix(senlist)
   
+  senlist[length(senlist)] <- str_trim(senlist[length(senlist)] ,"both")
+  
+  senlist[1] <- str_to_title(senlist[1])
+  
   sentence <- ""
   
   for (i in senlist){
     sentence <- paste(sentence, i, "")
   }
+  
+  sentence <- paste(sentence, ".", sep = "")
   
   return(sentence)
 }
@@ -133,22 +148,27 @@ body <- dashboardBody(
     
     tabItem(tabName = "dialogue",
             fluidRow(
-            box(textOutput("reprint")),
-            
-            box(
-              title = "Character Choice",
-              selectInput("char", "Character:", charnames)
-              ),
-            box(
-              title = "# of Words in The Sentence",
-              numericInput("senlength", "Sentence Length", 20, min = 2, max = 100)
+              box(textOutput("reprint"),title = "Generated Line:", solidHeader = T, background = "aqua"),
+              
+              box(
+                title = "Character Choice", solidHeader = T, background = "light-blue",
+                selectInput("char", "Character:", charnames)
+                )
+            ),
+            fluidRow(
+              box(expl1, br(), expl2, br(), expl3, title = "About This Model", background = "red"),
+              box(
+                title = "# of Words in The Sentence", solidHeader = T, background = "green",
+                numericInput("senlength", "Choose Between 2 and 100 words.", 20, min = 2, max = 100),
+                "The longer of line you generate, the more patient you are going to have to be with the model."
+              )
             )
-            )  
-    ),
+      ),  
     
     tabItem(tabName = "information")
   )
 )
+
 
 
 ui <-  dashboardPage(skin = "red",
